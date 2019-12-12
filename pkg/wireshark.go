88ad0d784c9f4f55c019e8af6246c5d125e0ffc3
maozhengwen <546166705@qq.com>
2019-12-12 17:55:38 +0800

    up[date

--- pkg/wireshark.go
+++ pkg/wireshark.go
@@ -72,7 +72,7 @@ func WireShark(deviceName string) {
 			srcPort = tcp.SrcPort.String()
 			dstPort = tcp.DstPort.String()
 		}
-		log.Infof("%s:%s  ->  %s:%s", srcIP, srcPort, dstIP, dstPort)
+		//log.Infof("%s:%s  ->  %s:%s", srcIP, srcPort, dstIP, dstPort)
 
 		applicationLayer := packet.ApplicationLayer()
 		if applicationLayer == nil {
@@ -110,7 +110,9 @@ func WireShark(deviceName string) {
 				}
 				udidTimestampIPPortMap.LoadOrStore(m["udid"][0]+"_"+m["timestamp"][0], srcIP+"_"+srcPort)
 				udidTimestampFileMap.LoadOrStore(m["udid"][0]+"_"+m["timestamp"][0], getFileSize(fileName))
-				iPPortFileMap.Store(srcIP+"_"+srcPort, 0)
+				log.Infof("udidTimestampIPPortMap(key:%v,value:%v)", m["udid"][0]+"_"+m["timestamp"][0], srcIP+"_"+srcPort)
+				log.Infof("udidTimestampFileMap(key:%v,value:%v)", m["udid"][0]+"_"+m["timestamp"][0], getFileSize(fileName))
+				//iPPortFileMap.Store(srcIP+"_"+srcPort, 0)
 			}
 		}
 
